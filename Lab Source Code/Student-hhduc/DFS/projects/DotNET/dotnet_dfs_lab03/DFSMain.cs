4d36906cd023b734b0909575d2617a33e66b5a70
Minh Hieu Nguyen <minhhi227@gmail.com>
2017-07-01 18:38:24 -0500

    vtb

---
+++ Lab Source Code/Student-hhduc/DFS/projects/DotNET/dotnet_dfs_lab03/DFSMain.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Emc.Documentum.FS.Runtime;
+
+namespace DotNETDFS
+{
+    class DFSMain
+    {
+        private String _repository;
+        private String _userName;
+        private String _password;
+
+        ///////////////////////////////////////////////////////////////////////////
+        // args[0] should be the repository name
+        // args[1] should be the user name
+        // args[2] should be the password
+        static void Main(string[] args)
+        {
+
+            if (args.Length < 3)
+            {
+                Console.Out.WriteLine("Missing required arguments!");
+                Console.Out.WriteLine("The required areguments are <repository> <user name> <password>");
+                return;
+            }
+
+            DFSMain main = new DFSMain();
+            main._repository = args[0];
+            main._userName = args[1];
+            main._password = args[2];
+
+            //Console.Out.WriteLine("repository = '" + main._repository + "'");
+            //Console.Out.WriteLine("user name = '" + main._userName + "'");
+            //Console.Out.WriteLine("password = '" + main._password + "'");
+
+            try
+            {
+                Console.Out.WriteLine("Starting Program...");
+                DFSLogic.startLogic(main._repository, main._userName, main._password);
+            }
+
+            catch (Exception e)
+            {
+                Console.Out.WriteLine(e.Message);
+                Console.Out.WriteLine(e.StackTrace);
+            }
+
+            Console.Out.WriteLine("Finished");
+        }
+
+        ///////////////////////////////////////////////////////////////////////////
+
+        public DFSMain()
+        {
+            this._repository = "";
+            this._userName = "";
+            this._password = "";
+        }
+        ///////////////////////////////////////////////////////////////////////////
+    }
+}
---
+++ Lab Source Code/Student-hhduc/DFS/projects/DotNET/dotnet_dfs_lab03_soln/DFSLogic.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Emc.Documentum.FS.Runtime.Context;
+using Emc.Documentum.FS.DataModel.Core;
+using Emc.Documentum.FS.DataModel.Core.Properties;
+using Emc.Documentum.FS.DataModel.Core.Context;
+using Emc.Documentum.FS.Services.Core;
+using Emc.Documentum.FS.DataModel.Core.Profiles;
+using Emc.Documentum.FS.DataModel.Core.Content;
+
+namespace DotNETDFS
+{
+    class DFSLogic
+    {
+
+        public static void startLogic(String repository, String userName, String password)
+        {
+            IServiceContext context = createContext(repository, userName, password);
+            
+            //comment block 'new service'
+
+            Console.Out.WriteLine("Creating new IObjectService instance...");
+
+            //create a service factory and instatiate the IObjectService
+            ServiceFactory sf = ServiceFactory.Instance;
+            IObjectService objectService =
+                sf.GetRemoteService<IObjectService>(context);
+
+            //comment block 'build'
+
+            Console.Out.WriteLine("Creating new ObjectIdentiy instance...");
+
+            //build object identity
+            ObjectIdentity docIdent = new ObjectIdentity();
+            docIdent.RepositoryName = repository;
+
+            Console.Out.WriteLine("Creating new DataObject instance...");
+
+            //build data object, seed with object identity
+            DataObject dataObj = new DataObject(docIdent, "dm_document");
+
+            Console.Out.WriteLine("Creating new PropertySet instance...");
+
+            //build property set
+            String objName = "lab03_test";
+            PropertySet propertySet = new PropertySet();
+            propertySet.Set("object_name", objName);
+
+            //seed data object with property set
+            dataObj.Properties = propertySet;
+
+            Console.Out.WriteLine("Creating new DataPackage instance...");
+
+            //build data package, seed with data object
+            DataPackage dataPack = new DataPackage(dataObj);
+
+            Console.Out.WriteLine("Calling the IObjectService.create() method...");
+
+            OperationOptions options = null;
+
+            //call the create method
+            objectService.Create(dataPack, options);
+
+            Console.Out.WriteLine("A new object called '" + objName + "' has been created...");
+
+        }
+
+        private static IServiceContext createContext(String repository, String userName, String password)
+        {
+            Console.Out.WriteLine("Creating new empty IServiceContext instance...");
+
+            //build context factory and an empty context
+            ContextFactory cf = ContextFactory.Instance;
+            IServiceContext context = cf.NewContext();
+
+            Console.Out.WriteLine("Creating new repository identity...");
+
+            //create a repository ID and seed it with the login credentials
+            RepositoryIdentity repoIdent = new RepositoryIdentity(repository, userName, password, "");
+
+            Console.Out.WriteLine("Adding repository ID into IServiceContext object...");
+
+            //seed the IServiceContext with the Repository ID
+            context.AddIdentity(repoIdent);
+
+            ContentTransferProfile ctp = new ContentTransferProfile();
+            ctp.TransferMode = ContentTransferMode.BASE64;
+            context.SetProfile(ctp);
+
+            return context;
+        }
+    }
+}
